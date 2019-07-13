8cd15eee007fdaeef7fe357ffdb79dc516e1e86c
Tommy Goris <goristommyucf@knights.ucf.edu>
2019-07-13 00:50:16 -0400

    Finally got GTK/Relm showing the window.

--- examples/genetic_algorithm_gui/main.rs
+++ examples/genetic_algorithm_gui/main.rs
@@ -1,12 +1,15 @@
-extern crate azul;
+extern crate gtk;
+#[macro_use]
+extern crate relm;
+#[macro_use]
+extern crate relm_derive;
 
-use azul::{
-    dom::NodeType::{Image, Text},
-    prelude::*,
-    widgets::text_input::{TextInput, TextInputState},
-    window::FakeWindow,
-    widgets::{label::Label, button::Button},
-};
+use gtk::prelude::*;
+use gtk::{Inhibit, Window, WindowType};
+use relm::{Relm, Update, Widget};
+
+use self::Msg::*;
+
 use genetic_algorithm::crossover::genome_crossover::StringCrossover;
 use genetic_algorithm::genome::population::{Individual, Population, ProblemType};
 use genetic_algorithm::genome::problem::{FitnessFunction, OneMax};
@@ -15,152 +18,85 @@ use genetic_algorithm::selection::genome_selection::TournamentSelection;
 use plotters::prelude::*;
 use rand::prelude::*;
 use rand::Rng;
+use std::env::args;
 use std::path::PathBuf;
 #[cfg(debug_assertions)]
 use std::time::Duration;
 
-macro_rules! css_path {
-    () => {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/src/app.css")
-    };
-}
-//macro_rules! FONT_PATH {() => { concat!(env!("CARGO_MANIFEST_DIR"), "/../assets/fonts/KoHo-Light.ttf")};}
-
-#[cfg(debug_assertions)]
-struct DataModel {
-    counter: usize,
-    text_input: TextInputState,
-    //label: Label,
+#[derive(Msg)]
+enum Msg {
+    // …
+    Quit,
 }
 
-impl Layout for DataModel {
-    fn layout(&self, mut _info: LayoutInfo<Self>) -> Dom<Self> {
-        //        let image_id = _info.resources.add_css_image_id("output");
-        //        #[cfg(feature = "image_loading")]
-        //        _info.resources.add_image(image_id, ImageSource::File(PathBuf::from("output.png")));
-        fn add_population_text_box(
-            window: &mut FakeWindow<DataModel>,
-            text_input: &TextInputState,
-            data_model: &DataModel,
-            label_text: String,
-        ) -> Dom<DataModel> {
-            let text_input = TextInput::new()
-                .bind(window, &text_input, &data_model)
-                .dom(&text_input)
-                .with_class("test");
+struct Model {
+    // …
+}
 
-            let label2 = Label::new(label_text)
-                .dom()
-                .with_class("left_align_label_text");
+struct Win {
+    // …
+    model: Model,
+    window: Window,
+}
 
-            let label_div = Dom::div()
-                .with_class("label_with_textbox")
-                .with_child(text_input)
-                .with_child(label2);
+impl Update for Win {
+    // Specify the model used for this widget.
+    type Model = Model;
+    // Specify the model parameter used to init the model.
+    type ModelParam = ();
+    // Specify the type of the messages sent to the update function.
+    type Msg = Msg;
 
-            Dom::div().with_child(label_div)
+    // Return the initial model.
+    fn model(_: &Relm<Self>, _: ()) -> Model {
+        Model {}
+    }
+
+    // The model may be updated when a message is received.
+    // Widgets may also be updated in this function.
+    fn update(&mut self, event: Msg) {
+        match event {
+            Msg::Quit => gtk::main_quit(),
         }
+    }
+}
 
-        fn add_chart_image(resources: &AppResources) -> Dom<DataModel> {
-            let image_id = *resources.get_css_image_id("output").unwrap();
-            let image_info = resources.get_image_info(&image_id);
-            //let image_bytes = resources.get_image_bytes(&image_id);
-            //            println!("{:?} {:?} {:?}", image_id, image_info, image_bytes);
-            Dom::div()
-                .with_class("image_div")
-                .with_child(Dom::image(image_id).with_id("chart"))
+impl Widget for Win {
+    // Specify the type of the root widget.
+    type Root = Window;
+
+    // Return the root widget.
+    fn root(&self) -> Self::Root {
+        self.window.clone()
+    }
+
+    // Create the widgets.
+    fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
+        // GTK+ widgets are used normally within a `Widget`.
+        let window = Window::new(WindowType::Toplevel);
+
+        // Connect the signal `delete_event` to send the `Quit` message.
+        connect!(
+            relm,
+            window,
+            connect_delete_event(_, _),
+            return (Some(Msg::Quit), Inhibit(false))
+        );
+        // There is also a `connect!()` macro for GTK+ events that do not need a
+        // value to be returned in the callback.
+
+        window.show_all();
+
+        Win {
+            model,
+            window: window,
         }
-        //add_population_text_box(_info, &self.text_input, &self)
-        Dom::div()
-            .with_class("orange")
-            .with_child(add_population_text_box(
-                _info.window,
-                &self.text_input,
-                &self,
-                String::from("Population:"),
-            ))
-            .with_child(add_population_text_box(
-                _info.window,
-                &self.text_input,
-                &self,
-                String::from("Crossover Rate:"),
-            ))
-            .with_child(add_population_text_box(
-                _info.window,
-                &self.text_input,
-                &self,
-                String::from("Mutation Rate:"),
-            ))
-            .with_child(add_population_text_box(
-                _info.window,
-                &self.text_input,
-                &self,
-                String::from("Selection Rate:"),
-            ))
-            .with_child(add_population_text_box(
-                _info.window,
-                &self.text_input,
-                &self,
-                String::from("Solution Fitness:"),
-            ))
-            .with_child(add_chart_image(_info.resources))
-        //.with_child(TextInput::new().dom(&self.text_input))
-        //.with_child(Dom::label(String::from("hello2")))
-        //.with_child(Dom::label(String::from("hello3")))
-        //        TextInput::new()
-        //            .bind(_info.window, &self.text_input, &self)
-        //            .dom(&self.text_input)
-        //            .with_class("test")
     }
 }
 
 fn main() {
-    create_chart();
-    let mut file_path = PathBuf::new();
-    file_path.push("C:\\Users\\goris\\CLionProjects\\genetic_algorithm\\examples\\genetic_algorithm_gui\\app.css");
-    let mut css = css::hot_reload_override_native(file_path, Duration::from_millis(100));
-    let mut app = App::new(
-        DataModel {
-            counter: 0,
-            text_input: TextInputState::default(),
-        },
-        AppConfig::default(),
-    )
-    .unwrap();
-
-    let image_id = app.app_state.resources.add_css_image_id("output");
-
-    app.app_state.resources.add_image_source(
-        image_id,
-        ImageSource::File(PathBuf::from(
-            "C:\\Users\\goris\\CLionProjects\\genetic_algorithm\\0.png",
-        )),
-    );
-
-    let window = app
-        .create_hot_reload_window(WindowCreateOptions::default(), css)
-        .unwrap();
-    app.run(window).unwrap();
-
-    //    let seed: &[u8; 32] = &[1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 3, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4];
-    //    let mut one_max_problem = OneMax::default();
-    //    let mut pop= init_string_pop(*seed, &mut one_max_problem);
-    //
-    //    //one_step(&mut pop, &mut crossover, &mut selection, &mut one_max_problem);
-    //
-    //    let mut selection = TournamentSelection::new(7, 0.80, *seed);
-    //    let mut crossover = StringCrossover::new(0.80, 7, *seed);
-    //    let mut mutation = StringMutation::new(0.05, vec!['0', '1'], *seed);
-    //    let max_fitness = &100.0;
-    //    loop {
-    //        pop.crossover(&mut crossover, &mut selection, Box::new(one_max_problem));
-    //        pop.mutate(&mut mutation, Box::new(one_max_problem));
-    //        let individual = pop.find_top_individual();
-    //        println!("{:?}", individual);
-    //        if individual.fitness() == max_fitness {
-    //            break;
-    //        }
-    //    }
+    //create_chart();
+    Win::run(()).expect("Win::run failed");
 }
 
 //fn one_step(population: &mut Population<String>) {
